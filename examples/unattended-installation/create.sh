b9a000814f721894ab08a02d089ff44e1a2702eb
Stephen Fox <stephen.j.fox.jr@gmail.com>
2023-09-02 15:46:24 -0400

    examples: Initial unattended installation example.

---
+++ examples/unattended-installation/create.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -u
+
+dir_path="$(realpath $(dirname "${0}"))"
+
+arch=amd64
+version=7.3
+
+doas mkobsd \
+  -o "${dir_path}/example-${version}-${arch}.iso" \
+  -a ${arch} \
+  -r ${version} \
+  -i "${dir_path}/auto_install.conf" \
+  -d "${dir_path}/generic"
---
+++ examples/unattended-installation/generic/usr/local/bin/this-ends-up-in-usr-local-bin.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This is just an example of creating a file in /usr/local/bin using the
+# install.site(5) automation.
+#
+# mkobsd will automatically include this file when install.site functionality
+# is specified.
+
+echo "hellooooooooooooooooooooooooooooooo oooooo ooo  oo"
